40d6318444ca27d9c7ca55ba8e28c09a4fef43cf
tnaydenov3 <59939223+tnaydenov3@users.noreply.github.com>
2023-12-06 16:37:14 +0200

    Update wiktionary_kanji.py

--- wiktionary_kanji.py
+++ wiktionary_kanji.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -17,10 +18,9 @@ def get_kanji_info(kanji):
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # Table of content
-        kj_rel_variants = ''
-        kj_alt_forms = ''
         kj_radical = ''
         kj_composition = ''
+        kj_disambig = ''
 
         kj_zhuyin = ''
         kj_kana = ''
@@ -34,15 +34,14 @@ def get_kanji_info(kanji):
         mrk_korean = soup.find('span', {'id': ID_KOREAN})
 
         # Sections
-        sctn_translingual = BeautifulSoup(extract_section_content(mrk_translingual), 'html.parser') if mrk_translingual else None
-        sctn_chinese = BeautifulSoup(extract_section_content(mrk_chinese), 'html.parser') if mrk_chinese else None
-        sctn_japanese = BeautifulSoup(extract_section_content(mrk_japanese), 'html.parser') if mrk_japanese else None
-        sctn_korean = BeautifulSoup(extract_section_content(mrk_korean), 'html.parser') if mrk_korean else None
+        soup_translingual = BeautifulSoup(extract_section_content(mrk_translingual), 'html.parser') if mrk_translingual else None
+        soup_chinese = BeautifulSoup(extract_section_content(mrk_chinese), 'html.parser') if mrk_chinese else None
+        soup_japanese = BeautifulSoup(extract_section_content(mrk_japanese), 'html.parser') if mrk_japanese else None
+        soup_korean = BeautifulSoup(extract_section_content(mrk_korean), 'html.parser') if mrk_korean else None
 
         # Get writing data
-        kj_rel_variants = get_related_variants(sctn_translingual)
-        kj_alt_forms = get_alt_forms(sctn_translingual)
-        kj_radical, kj_composition = get_radical_and_composition(sctn_translingual)
+        kj_radical, kj_composition = get_radical_and_composition(soup_translingual)
+        kj_disambig = get_disambig(soup)
 
         # Get reading data
         # kj_zhuyin = get_zhuyin()
@@ -50,39 +49,36 @@ def get_kanji_info(kanji):
         # kj_jyutping = get_jyutping()
         # kj_hangul = get_hangul()
         
-        kanji_info = [kanji, kj_rel_variants, kj_alt_forms, kj_radical, kj_composition, kj_zhuyin, kj_kana, kj_jyutping, kj_hangul]        
+        kanji_info = [kanji, kj_radical, kj_composition, kj_disambig, kj_zhuyin, kj_kana, kj_jyutping, kj_hangul]        
         print(kanji_info)
 
-        return kanji_info
-    
+        return '\t'.join(kanji_info)
+        
     else:
         return f'Failed to retrieve information for {kanji}. Status code: {response.status_code}'
-    
-def get_related_variants(sctn_translingual):
-    el_related_chars = sctn_translingual.find('span', {'id': 'Related_characters'})
-    if el_related_chars:
-        vars_list = []
-        for li_el in el_related_chars.find_next('ul').find_all('li'):
-            vars_list += [li_el.find('span').text.strip()]
-        kj_rel_variants = ' '.join(vars_list)
-    return kj_rel_variants
 
-def get_alt_forms(sctn_translingual):
-    kj_alt_forms = ''
-    el_alt_forms = sctn_translingual.find('span', {'id': 'Alternative_forms'})
-    if el_alt_forms:
-        print(el_alt_forms.text.strip())
-        alt_list = []
-        for li_el in el_alt_forms.find_next('ul').find_all('li'):
-            alt_list += [li_el.find('span').text.strip()]
-        kj_alt_forms = ' '.join(alt_list)    
-    return kj_alt_forms
+def get_disambig(soup):
+    kj_disambig = ''
+    disambig_list = []
 
-def get_radical_and_composition(sctn_translingual):
-    el_han_char = sctn_translingual.find('span', {'id': 'Han_character'})
-    p_text = el_han_char.find_next('p').text.strip()
-    kj_radical = p_text.split('(')[1].split('+')[0].strip().replace(',',':')
-    kj_composition = p_text.split('composition ')[-1].replace(')','').strip() if 'composition' in p_text else ''
+    disambig_div = soup.find('div', {'class': re.compile(r'^disambig-see-also')})
+    if disambig_div:
+        for b_el in disambig_div.find_all('b', {'class': 'Hani'}):
+            b_text = b_el.text.strip()
+            if not b_text in disambig_list:
+                disambig_list += [b_text]
+        kj_disambig = ' '.join(disambig_list)
+    return kj_disambig
+
+def get_radical_and_composition(soup_translingual):
+    mrk_han_char = soup_translingual.find('span', {'id': 'Han_character'})
+    p_text = mrk_han_char.find_next('p').text.strip()
+
+    kj_radical = p_text.split('(')[1].split('+')[0].strip().replace(',',':').replace('Kangxi radical','Kangxi')
+
+    kj_composition = p_text.split('composition ')[-1].strip() if 'composition' in p_text else ''
+    kj_composition = re.sub(r'\([^)]*\)', '', kj_composition).split(')')[0]
+
     return kj_radical, kj_composition
 
 def extract_section_content(start_marker):
@@ -96,7 +92,10 @@ def extract_section_content(start_marker):
         current_element = current_element.find_next()
 
     return ''.join(content)
-    
+
+def is_kanji(char):
+    return bool(re.match(r'^[\u4e00-\u9faf]$', char))
+
 def main():
     input_filename = 'input.txt'
     output_filename = 'output.txt'
@@ -105,7 +104,7 @@ def main():
         for line in input_file:
             kanji = line.strip()
             kanji_info = get_kanji_info(kanji)
-            output_file.write(f'{kanji} - Output:\t{kanji_info}\n')
+            output_file.write(f'{kanji} output:\t{kanji_info}\n')
 
     print(f'Kanji information has been saved to {output_filename}')
 
