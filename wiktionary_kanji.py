d1b30e734a7b7f2782af5504c7def34eb5471dac
tnaydenov3 <59939223+tnaydenov3@users.noreply.github.com>
2023-12-06 11:37:20 +0200

    Update wiktionary_kanji.py

--- wiktionary_kanji.py
+++ wiktionary_kanji.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+ID_TRANSLINGUAL = 'Translingual'
+ID_CHINESE = 'Chinese'
+ID_JAPANESE = 'Japanese'
+ID_KOREAN = 'Korean'
+
 def get_kanji_info(kanji):
     url = f'https://en.wiktionary.org/wiki/{kanji}'
 
@@ -11,68 +16,86 @@ def get_kanji_info(kanji):
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Define variables with placeholder values
-        kj_variants = ''
+        # Table of content
+        kj_rel_variants = ''
         kj_alt_forms = ''
-
         kj_radical = ''
         kj_composition = ''
 
         kj_zhuyin = ''
         kj_kana = ''
+        kj_jyutping = ''
+        kj_hangul = ''
 
         # Span markers
-        mrk_translingual = soup.find('span', {'id': 'Translingual'})
-        mrk_chinese = soup.find('span', {'id': 'Chinese'})
-        mrk_japanese = soup.find('span', {'id': 'Japanese'})
+        mrk_translingual = soup.find('span', {'id': ID_TRANSLINGUAL})
+        mrk_chinese = soup.find('span', {'id': ID_CHINESE})
+        mrk_japanese = soup.find('span', {'id': ID_JAPANESE})
+        mrk_korean = soup.find('span', {'id': ID_KOREAN})
 
         # Sections
-        sctn_translingual = extract_section_content(mrk_translingual)
-        sctn_chinese = extract_section_content(mrk_chinese)
-        sctn_japanese = extract_section_content(mrk_japanese)
+        sctn_translingual = BeautifulSoup(extract_section_content(mrk_translingual), 'html.parser') if mrk_translingual else None
+        sctn_chinese = BeautifulSoup(extract_section_content(mrk_chinese), 'html.parser') if mrk_chinese else None
+        sctn_japanese = BeautifulSoup(extract_section_content(mrk_japanese), 'html.parser') if mrk_japanese else None
+        sctn_korean = BeautifulSoup(extract_section_content(mrk_korean), 'html.parser') if mrk_korean else None
 
-        el_alt_forms = soup.find('span', {'id': 'Alternative_forms'})
-        if el_alt_forms:
-            print(el_alt_forms.text.strip())
-            alt_list = []
-            for li_el in el_alt_forms.find_next('ul').find_all('li'):
-                alt_list += [li_el.find('span').text.strip()]
-            kj_alt_forms = ' '.join(alt_list)
+        # Get writing data
+        kj_rel_variants = get_related_variants(sctn_translingual)
+        kj_alt_forms = get_alt_forms(sctn_translingual)
+        kj_radical, kj_composition = get_radical_and_composition(sctn_translingual)
 
-        el_han_char = soup.find('span', {'id': 'Han_character'})
-        p_text = el_han_char.find_next('p').text.strip()
-        kj_radical = p_text.split('(')[1].split('+')[0].strip().replace(',',':')
-        kj_composition = p_text.split('composition ')[-1].replace(')','').strip() if 'composition' in p_text else ''
-
-        el_related_chars = soup.find('span', {'id': 'Related_characters'})
-        if el_related_chars:
-            vars_list = []
-            for li_el in el_related_chars.find_next('ul').find_all('li'):
-                vars_list += [li_el.find('span').text.strip()]
-            kj_variants = ' '.join(vars_list)
+        # Get reading data
+        # kj_zhuyin = get_zhuyin()
+        # kj_kana = get_kana()
+        # kj_jyutping = get_jyutping()
+        # kj_hangul = get_hangul()
         
-        kanji_info = [kanji, kj_variants, kj_alt_forms, kj_radical, kj_composition, kj_zhuyin, kj_kana]        
+        kanji_info = [kanji, kj_rel_variants, kj_alt_forms, kj_radical, kj_composition, kj_zhuyin, kj_kana, kj_jyutping, kj_hangul]        
         print(kanji_info)
 
         return kanji_info
-
+    
     else:
         return f'Failed to retrieve information for {kanji}. Status code: {response.status_code}'
-        
-def extract_section_content(start_element):
-    end_element = start_element.find_next('h2')
+    
+def get_related_variants(sctn_translingual):
+    el_related_chars = sctn_translingual.find('span', {'id': 'Related_characters'})
+    if el_related_chars:
+        vars_list = []
+        for li_el in el_related_chars.find_next('ul').find_all('li'):
+            vars_list += [li_el.find('span').text.strip()]
+        kj_rel_variants = ' '.join(vars_list)
+    return kj_rel_variants
 
-    if start_element and end_element:
-        content = []
-        current_element = start_element.find_next()
+def get_alt_forms(sctn_translingual):
+    kj_alt_forms = ''
+    el_alt_forms = sctn_translingual.find('span', {'id': 'Alternative_forms'})
+    if el_alt_forms:
+        print(el_alt_forms.text.strip())
+        alt_list = []
+        for li_el in el_alt_forms.find_next('ul').find_all('li'):
+            alt_list += [li_el.find('span').text.strip()]
+        kj_alt_forms = ' '.join(alt_list)    
+    return kj_alt_forms
 
-        while current_element and current_element != end_element:
-            content.append(str(current_element))
-            current_element = current_element.find_next()
+def get_radical_and_composition(sctn_translingual):
+    el_han_char = sctn_translingual.find('span', {'id': 'Han_character'})
+    p_text = el_han_char.find_next('p').text.strip()
+    kj_radical = p_text.split('(')[1].split('+')[0].strip().replace(',',':')
+    kj_composition = p_text.split('composition ')[-1].replace(')','').strip() if 'composition' in p_text else ''
+    return kj_radical, kj_composition
 
-        return ''.join(content)
-    else:
-        return None
+def extract_section_content(start_marker):
+    start_element = start_marker.find_parent('h2')
+    end_element_tag='h2'
+    content = []
+    current_element = start_element.find_next()
+
+    while current_element and current_element.name != end_element_tag:
+        content.append(str(current_element))
+        current_element = current_element.find_next()
+
+    return ''.join(content)
     
 def main():
     input_filename = 'input.txt'
